53fad80049ce61907c89f9647902575c9ee927b6
Rumen Manev <rumen.manev@mova.ie>
2023-03-29 01:16:33 +0300

    loading images + loading animation

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Dog } from "../../types/Dog";
+import rottweiler from "../../assets/rottweiler.svg";
+import dachshund from "../../assets/dachs.svg";
+import greyhound from "../../assets/greyhound.svg";
+import bulldog from "../../assets/bulldog.svg";
+import running from "../../assets/running.gif";
 
 type SliderProps = {
   dogs: Dog[];
@@ -9,6 +14,7 @@ type SliderProps = {
 const Slider = ({ dogs }: SliderProps) => {
   const [loading, setLoading] = useState<boolean>(true);
   const [activeIndex, setActiveIndex] = useState(0);
+  const [dogSlides, setDogSlides] = useState<any[]>([]);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -27,20 +33,52 @@ const Slider = ({ dogs }: SliderProps) => {
     }
   }, [dogs]);
 
+  // useEffect(() => {
+  //   if (dogs.length > 0) {
+  //     const filteredData = dogs.filter((item) => item.name && item.temperament);
+  //     setDogSlides((prevState) => [...prevState, ...filteredData]);
+
+  //     const dogImages = [
+  //       { id: 1, src: rottweiler },
+  //       { id: 2, src: dachshund },
+  //       { id: 3, src: bulldog },
+  //       { id: 4, src: greyhound },
+  //     ];
+  //     setDogSlides((prevState) => [...prevState, ...dogImages]);
+  //     console.log(dogSlides);
+  //   }
+  // }, [dogs]);
+
   return (
     <SliderContainer>
       {loading ? (
-        // <Lottie options={defaultOptions} />
-        <p>Loading...</p>
+        <LoadingContainer>
+          <LoadingAnimation src={running} />
+          <LoadingText>Fetching your data...</LoadingText>
+        </LoadingContainer>
       ) : (
         <CarouselWrapper>
-          {dogs.map((dog, index) => (
-            <SlideWrapper key={index} active={index === activeIndex}>
-              {/* <Slide src={dog} alt={`Slide ${index + 1}`} /> */}
-              <SlideTitle>{dog.name}</SlideTitle>
-              <SlideText>{dog.temperament}</SlideText>
-            </SlideWrapper>
-          ))}
+          {dogs.map((dog, index) => {
+            let dogImage = "";
+            for (let i = 0; i < dogs.length; i++) {
+              if (dog.name.includes("Rottwe")) {
+                dogImage = rottweiler;
+              } else if (dog.name.includes("Dachsh")) {
+                dogImage = dachshund;
+              } else if (dog.name.includes("American Bulld")) {
+                dogImage = bulldog;
+              } else if (dog.name.includes("Greyh")) {
+                dogImage = greyhound;
+              }
+            }
+            return (
+              <SlideWrapper key={index} active={index === activeIndex}>
+                <SlideImage src={dogImage} alt={`Slide ${index + 1}`} />
+                <SlideTitle>{dog.name}</SlideTitle>
+                <SlideText>{dog.temperament}</SlideText>
+              </SlideWrapper>
+            );
+          })}
           <DotsWrapper>
             {dogs.map((dog, index) => (
               <Dot
@@ -58,11 +96,35 @@ const Slider = ({ dogs }: SliderProps) => {
 
 export default Slider;
 
-const SliderContainer = styled.div``;
+const LoadingContainer = styled.div`
+  margin-top: 50%;
+`;
+
+const LoadingAnimation = styled.img``;
+
+const LoadingText = styled.p`
+  font-family: "Nunito", sans-serif;
+`;
+
+const SlideImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  top: 0;
+  left: 0;
+  z-index: 0;
+`;
+
+const SliderContainer = styled.div`
+  width: 50%;
+  margin: auto;
+  text-align: center;
+`;
 
 const CarouselWrapper = styled.div`
   width: 100%;
   height: 400px;
+  margin-top: 20%;
   position: relative;
 `;
 
@@ -96,21 +158,9 @@ const Dot = styled.button`
 `;
 
 const SlideTitle = styled.h2`
-  // width: 100%;
-  // height: 100%;
-  // font-size: 50px;
-  // position: absolute;
-  // text-align: center;
-  // top: 40%;
-  // z-index: 10;
+  font-family: "Nunito", sans-serif;
 `;
 
-const SlideText = styled.h3`
-  // width: 100%;
-  // height: 100%;
-  // position: absolute;
-  // text-align: center;
-  // z-index: 10;
-  // top: 65%;
-  // font-size: 2rem;
+const SlideText = styled.p`
+  font-family: "Nunito", sans-serif;
 `;
